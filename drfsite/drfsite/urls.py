98cc34fdd14acfd7d2a578e62e78ae0306c82f44
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2022-11-14 17:42:57 +0300

    lesson #8 completed

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from footballers.views import *
 
+router = routers.SimpleRouter()
+router.register(r'footballer', FootballerViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/footballerslist', FootballerAPIList.as_view()),
-    path('api/v1/footballerslist/<int:pk>', FootballerAPIUpdate.as_view()),
-    path('api/v1/footballersdetail/<int:pk>', FootballerAPIDetailView.as_view()),
+    path('api/v1/', include(router.urls))  # http://127.0.0.1:8000/api/v1/footballer
+    # path('api/v1/footballerslist', FootballerViewSet.as_view({'get': 'list'})),
+    # path('api/v1/footballerslist/<int:pk>', FootballerViewSet.as_view({'put': 'update'})),
 ]
--- drfsite/footballers/views.py
+++ drfsite/footballers/views.py
@@ -1,5 +1,5 @@
 from django.forms import model_to_dict
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,17 +7,20 @@ from rest_framework.views import APIView
 from .models import Footballer
 from .serializers import FootballerSerializer
 
-
-class FootballerAPIList(generics.ListCreateAPIView):
+class FootballerViewSet(viewsets.ModelViewSet):
     queryset = Footballer.objects.all()
     serializer_class = FootballerSerializer
 
-
-class FootballerAPIUpdate(generics.UpdateAPIView):
-    queryset = Footballer.objects.all()
-    serializer_class = FootballerSerializer
-
-
-class FootballerAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Footballer.objects.all()
-    serializer_class = FootballerSerializer
+# class FootballerAPIList(generics.ListCreateAPIView):
+#     queryset = Footballer.objects.all()
+#     serializer_class = FootballerSerializer
+#
+#
+# class FootballerAPIUpdate(generics.UpdateAPIView):
+#     queryset = Footballer.objects.all()
+#     serializer_class = FootballerSerializer
+#
+#
+# class FootballerAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Footballer.objects.all()
+#     serializer_class = FootballerSerializer
