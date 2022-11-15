32f3fdbfd4bc89bfd6750f8fa64f7a5af7a1be7f
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2022-11-15 10:15:15 +0300

    lesson #9 completed

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -19,8 +19,25 @@ from rest_framework import routers
 
 from footballers.views import *
 
-router = routers.SimpleRouter()
-router.register(r'footballer', FootballerViewSet)
+
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get': 'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix': 'List'}),
+        routers.Route(url=r'^{prefix}/{lookup}$',
+                      mapping={'get': 'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix': 'Detail'})
+    ]
+
+
+router = MyCustomRouter()
+router.register(r'footballer', FootballerViewSet, basename='footballer')
+print(*router.urls, sep='\n')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- drfsite/footballers/views.py
+++ drfsite/footballers/views.py
@@ -1,26 +1,33 @@
 from django.forms import model_to_dict
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, mixins
 from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 
-from .models import Footballer
+from .models import Footballer, Category
 from .serializers import FootballerSerializer
 
-class FootballerViewSet(viewsets.ModelViewSet):
+
+class FootballerViewSet(mixins.CreateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        mixins.ListModelMixin,
+                        GenericViewSet):
     queryset = Footballer.objects.all()
     serializer_class = FootballerSerializer
 
-# class FootballerAPIList(generics.ListCreateAPIView):
-#     queryset = Footballer.objects.all()
-#     serializer_class = FootballerSerializer
-#
-#
-# class FootballerAPIUpdate(generics.UpdateAPIView):
-#     queryset = Footballer.objects.all()
-#     serializer_class = FootballerSerializer
-#
-#
-# class FootballerAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Footballer.objects.all()
-#     serializer_class = FootballerSerializer
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        if not pk:
+            return Footballer.objects.all()[:3]
+        else:
+            return Footballer.objects.filter(pk=pk)
+
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
