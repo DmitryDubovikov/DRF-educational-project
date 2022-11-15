ff04fe05fe3929b3ab6ab29cb4648022de3e046a
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2022-11-15 18:14:07 +0300

    lesson #15 completed

--- drfsite/footballers/views.py
+++ drfsite/footballers/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, viewsets, mixins
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,20 +14,27 @@ from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import FootballerSerializer
 
 
+class FootballerAPIListPagination(PageNumberPagination):
+    page_size = 4
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class FootballerAPIList(generics.ListCreateAPIView):
     queryset = Footballer.objects.all()
     serializer_class = FootballerSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = FootballerAPIListPagination
 
 
 class FootballerAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Footballer.objects.all()
     serializer_class = FootballerSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
     # authentication_classes = (TokenAuthentication, )
 
 
 class FootballerAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Footballer.objects.all()
     serializer_class = FootballerSerializer
-    permission_classes = (IsAdminOrReadOnly, )
+    permission_classes = (IsAdminOrReadOnly,)
